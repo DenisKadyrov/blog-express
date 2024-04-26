14e24c1a9556b4988a0348846f771cf88fb8c25b
Denis <kripmrx@gmail.com>
2024-04-26 16:57:14 +0200

    not correct path to files in ./utils/index.js

--- utils/index.js
+++ utils/index.js
@@ -1,5 +1,5 @@
 /**
  * файл используется для того чтобы не нагружать /index.js роутами
  */
-export { default as checkAuth } from './utils/checkAuth.js';
-export { default as handleValidationErrors } from './utils/handleValidationErrors.js';
+export { default as checkAuth } from './checkAuth.js';
+export { default as handleValidationErrors } from './handleValidationErrors.js';
